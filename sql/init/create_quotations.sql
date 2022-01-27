533e923e74f54e54d2f430aa81116c2385ca741b
PAULO SERGIO DIAS CHAVES <paulochaves.keyrus@sulamerica.com.br>
2022-01-26 23:24:00 -0300

    chore: add create_quotations query

---
+++ sql/init/create_quotations.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS {tablename} (
+    quotation_id             INTEGER,
+    createdAt                DATE,
+    user_id                  INTEGER,
+    professionalActivity_id  INTEGER
+    broker_id                INTEGER
+);
